46d13d1c49711ccb19ec9b171710afedf93de073
Pastel26 <85456902+Pastel26@users.noreply.github.com>
2021-09-11 17:54:56 +0100

    Update batery-low.sh

--- Scripts/batery-low.sh
+++ Scripts/batery-low.sh
@@ -1,19 +1,5 @@
 #!bin/bash
 
-pid="/var/run/get_hosts.pid"
-trap "rm -f $pid" SIGSEGV
-trap "rm -f $pid" SIGINT
-
-if [ -e $pid ]; then
-    exit # pid file exists, another instance is running, so now we politely exit
-else
-    echo $$ > $pid # pid file doesn't exit, create one and go on
-fi
-
-# your normal workflow here...
-
-while true
-        do
 POWERSUPPLY="/sys/class/power_supply/ACAD/online" # could be different on your system!
 TOO_LOW=15 # how low is too low?
 NOT_CHARGING="0"
@@ -27,6 +13,3 @@ if [ $BATTERY_LEVEL -le $TOO_LOW -a $STATUS = $NOT_CHARGING ]
 then
         /bin/dunstify -u critical -r 69420 --icon=<path-to-icon> 'Bateria Fraca!! Carregar imediatamente'
 fi
-
-sleep 300 # 5minutes
-done
